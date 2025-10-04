eaaab787e1b8c2223bc9e851e1d765d897aa380b
afsarzan <afsar_zan@yahoo.com>
2025-10-04 15:52:39 -0700

    Chore: Migrating to nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'ProjectFlow - Project Management App',
+  description: 'Web site created with Next.js.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+        <head>
+            <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        </head>
+        <body>
+            <div id="root">{children}</div>
+            <script type="module" src="/src/main.tsx"></script>
+        </body>
+    </html>
+  )
+}
