5631ef9572887a32ff122e271f1685b9f0d4d416
afsarzan <afsar_zan@yahoo.com>
2025-10-04 23:06:20 -0700

    chore: Migration to nextjs completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import '../index.css';
 
 export const metadata: Metadata = {
   title: 'ProjectFlow - Project Management App',
@@ -15,7 +16,7 @@ export default function RootLayout({
         <head>
             <link rel="icon" type="image/svg+xml" href="/vite.svg" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </head>
         <body>
